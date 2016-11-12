df218df1a8dd247b21ef9f6fd260037ae11296b4
txus <jsus.90@gmail.com>
2016-11-12 16:42:39 +0100

    Added submodule for bash git prompt and uninstall file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -r .vim ~/
+cp -r .bash-git-prompt ~/
 cp .vimrc ~/
 cp .bashrc ~/
 cp .tmux.conf ~/
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf .vim
+rm -rf .bash-git-prompt
+rm -rf .vimrc
+rm -rf .bashrc
+rm -rf .tmux.conf
