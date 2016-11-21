2ffae4fc28e4282e19b879ace4d1062e8b75a00f
txus <jsus.90@gmail.com>
2016-11-21 18:21:11 +0100

    Removing airline cfg

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ cp .vimrc ~/
 cp .bashrc ~/
 cp .tmux.conf ~/
 cp .zshrc ~/
-cp .zshline ~/
-cp .tmuxline ~/
 mkdir -p ~/.config/powerline/themes/tmux
 cp .config/powerline/themes/tmux/default.json ~/.config/powerline/themes/tmux
 ./fonts/install.sh
